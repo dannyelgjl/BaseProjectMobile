25b0a72bd65ad64f9d3e3f28235a73a871239eea
dannyelgjl <dannyelgjl@gmail.com>
2022-10-27 10:45:36 -0300

    feat: config svg icon and testing components

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
-
+import HomeIcon from '../../assets/icons/home.svg';
+import THEME from '../../theme/theme';
 import {Container} from './styles';
 
 const Home: React.FC = () => {
-  return <Container />;
+  return (
+    <Container>
+      <HomeIcon width={40} height={40} color={THEME.NEUTRAL.BLACK} />
+    </Container>
+  );
 };
 
 export default Home;
